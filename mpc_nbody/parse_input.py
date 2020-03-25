0548c406831080738c600d1694a018b7fa34bc53
Mike Alexandersen (on Victoria) <mikea@asiaa.sinica.edu.tw>
2020-03-25 17:27:43 -0400

    Add functionality to parse_input to save elements in format appropriate for input into holman's reboundx

--- mpc_nbody/parse_input.py
+++ mpc_nbody/parse_input.py
@@ -2,19 +2,19 @@
 # mpc_nbody/mpc_nbody/parse_input.py
 
 '''
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------
 mpc_nbody's module for parsing OrbFit + ele220 elements
 
 Mar 2020
 Mike Alexandersen & Matthew Payne
 
 This module provides functionalities to
-(a) read an OrbFit .fel/.eq file containing heliocentric ecliptic cartesian els
+(a) read an OrbFit .fel/.eq file with heliocentric ecliptic cartesian els
 (b) read ele220 element strings
 (c) convert the above to barycentric equatorial cartesian elements
 
 This is meant to prepare the elements for input into the n-body integrator
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------
 '''
 
 # Import third-party packages
@@ -35,19 +35,19 @@ class ParseElements():
     Class for parsing elements and returning them in the correct format.
     '''
 
-    def __init__(self, inputfile=None, filetype=None):
+    def __init__(self, input_file=None, filetype=None):
         #If input filename provided, process it:
-        if isinstance(inputfile, str):
+        if isinstance(input_file, str):
             if filetype == 'ele220':
-                self.parse_ele220(inputfile)
-            if filetype == 'fel' | filetype == 'eq':
-                self.parse_orbfit(inputfile)
+                self.parse_ele220(input_file)
+            if (filetype == 'fel') | (filetype == 'eq'):
+                self.parse_orbfit(input_file)
             self.save_elements()
 
     def _get_Covariance_List(self, Els):
         '''
-        Convenience function for reading and splitting the covariance lines
-        of an OrbFit file.
+        Convenience function for reading and splitting the covariance
+        lines of an OrbFit file.
         Not intended for user usage.
         '''
         ElCov = []
@@ -71,11 +71,32 @@ class ParseElements():
         return (covErr, c11, c12, c13, c14, c15, c16, c22, c23, c24, c25, c26,
                 c33, c34, c35, c36, c44, c45, c46, c55, c56, c66)
 
+    def save_elements(self, output_file='holman_ic'):
+        """Save the barycentric equatorial cartesian elements to file."""
+        ### For now, just fake it!
+        self.barycentric_equatorial_cartesian_elements = _get_junk_data()
+        self.tstart = 2458849.5
+        # Below should be good
+        outfile = open(output_file, 'w')
+        outfile.write(f"tstart {self.tstart:}\n")
+        outfile.write("tstep +20.0\n")
+        outfile.write("trange 600.\n")
+        outfile.write("geocentric 0\n")
+        outfile.write("state\n")
+        els = self.barycentric_equatorial_cartesian_elements
+        for prefix in ['', 'd']:
+            for el in ['x_BaryEqu', 'y_BaryEqu', 'z_BaryEqu']:
+                outfile.write(f"{els[prefix + el]:e} ")
+            outfile.write("\n")
+
     def parse_ele220(self, ele220file=None):
         '''
         Parse a file containing a single ele220 line.
         Currently returns junk data.
         '''
+        if felfile is None:
+            raise TypeError ("Required argument 'ele220file'"
+                             " (pos 1) not found")
         self.barycentric_ecliptic_keplarian_elements
         return 0, 0, 0, 0, 0, 0
 
@@ -92,6 +113,9 @@ class ParseElements():
         --------
 `        Heliocentric ecliptic cartesian coordinates and epoch.
         '''
+        if felfile is None:
+            raise TypeError ("Required argument 'felfile' (pos 1) not found")
+
         obj = {}
         el = open(felfile).readlines()
         cart_head = '! Cartesian position and velocity vectors\n'
@@ -130,5 +154,24 @@ class ParseElements():
 # Functions
 # -----------------------------------------------------------------------------
 
+def _get_junk_data():
+    """Just make some junk data for saving."""
+    junk = {}
+    junk.update({'x_BaryEqu' : float(3), 'dx_BaryEqu' : float(0.3),
+                 'y_BaryEqu' : float(2), 'dy_BaryEqu' : float(0.2),
+                 'z_BaryEqu' : float(1), 'dz_BaryEqu' : float(0.1)})
+    junk.update({'sigma_x_BaryEqu' : 0.03, 'sigma_dx_BaryEqu' : 0.003,
+                 'sigma_y_BaryEqu' : 0.02, 'sigma_dy_BaryEqu' : 0.002,
+                 'sigma_z_BaryEqu' : 0.01, 'sigma_dz_BaryEqu' : 0.001}
+                )
+    junk.update({'x_y_BaryEqu' : 0.41, 'x_z_BaryEqu' : 0.42,
+                 'x_dx_BaryEqu' : 0.43, 'x_dy_BaryEqu' : 0.44,
+                 'x_dz_BaryEqu' : 0.45, 'y_z_BaryEqu' : 0.46,
+                 'y_dx_BaryEqu' : 0.47, 'y_dy_BaryEqu' : 0.48,
+                 'y_dz_BaryEqu' : 0.49, 'z_dx_BaryEqu' : 0.50,
+                 'z_dy_BaryEqu' : 0.51, 'z_dz_BaryEqu' : 0.52,
+                 'dx_dy_BaryEqu' : 0.53, 'dx_dz_BaryEqu' : 0.54,
+                 'dy_dz_BaryEqu' : 0.55})
+    return junk
 
 # End
